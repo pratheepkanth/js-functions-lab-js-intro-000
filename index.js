655eef727100addaa400cee34212e2187644b023
pratheevan kasirajah <pratheepkanth@yahoo.com>
2018-07-16 22:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name) {
   return `Happy holiday, ${name}!`
 }
 
-function happyHolidaysTo(holiday,name) {
+function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
